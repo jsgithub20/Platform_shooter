c221d929dad8fc130182c2b06f642db769f16849
jsgithub20 <niufortune@163.com>
2021-09-08 18:32:03 +0800

    updated

--- Code/xie/splat_gen.py
+++ Code/xie/splat_gen.py
@@ -1,17 +1,16 @@
 import csv
 import os
-import random
 
 import pygame as pg
 import pygame.freetype as ft
-from random import randint
+from random import randint, choice
 
 from typing import Optional
 
 # -----------------------------------------------------------------------------
 # Constants and global variables
 # -----------------------------------------------------------------------------
-VER = "Rev 0.2"
+VER = "Splat Generator V0.2"
 FPS = 60
 WIDTH = 1024
 HEIGHT = 768
@@ -26,11 +25,10 @@ class DrawText(pg.sprite.Sprite):
     def __init__(self, word_lst, font_lst, x, y):
         super().__init__()
         self.word_lst = word_lst
-        self.text = random.choice(self.word_lst)
+        self.text = choice(self.word_lst)
         self.fg_color = [(237, 105, 27), (166, 214, 201), (250, 185, 10)]
         self.bg_color = (0, 0, 0)
         self.font = ft.Font(FONT_PATH + font_lst[randint(0, (len(font_lst)-1))], 80)
-        # self.font.underline = True if randint(0, 1) else False
         self.rendered_ft = self.font.render(self.text, fgcolor=self.fg_color[randint(0, 2)],
                                             rotation=randint(-5, 5))
         self.image = self.rendered_ft[0]
@@ -58,21 +56,13 @@ class Game:
         self.running = True
         self.playing = True
         self.clock = pg.time.Clock()
-        # self.screen = pg.display.set_mode((WIDTH, HEIGHT), flags=pg.NOFRAME)
-        self.screen = pg.display.set_mode(flags=pg.NOFRAME | pg.FULLSCREEN | pg.HWSURFACE | pg.DOUBLEBUF)
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT), flags=pg.NOFRAME)
+        # self.screen = pg.display.set_mode(flags=pg.NOFRAME | pg.FULLSCREEN | pg.HWSURFACE | pg.DOUBLEBUF)
         self.screen_size = pg.display.get_window_size()
         self.screen_w = self.screen_size[0]
         self.screen_h = self.screen_size[1]
         self.screen_centerx = int(self.screen_w / 2)
         self.screen_centery = int(self.screen_h / 2)
-        self.one_third_y = int(self.screen_h / 3)
-        self.one_third_y_top = (0, 0, self.screen_w, self.one_third_y)
-        self.one_third_y_mid = (0, self.one_third_y, self.screen_w, self.one_third_y)
-        self.one_third_y_btm = (0, self.one_third_y*2, self.screen_w, self.screen_h-self.one_third_y*2)
-        self.styles = ['underline', 'strong', 'oblique', 'wide', 'kerning', 'vertical']
-        self.text = 'Freetype Fonts'
-        self.font_name = "resources/OvOV20.ttf"
-        self.font = ft.Font(self.font_name, 50)
         self.splat_font = ft.Font(FONT_PATH + "earwig factory rg.ttf", 80)
         self.any_key_font = ft.SysFont("calibri", 30)
         self.amy_font = ft.SysFont("calibri", 15)
@@ -82,11 +72,11 @@ class Game:
         self.file_saved_flg = False
         self.file_saved_cnt = 1
         self.file_saved_name = ""
-        self.word_lst = self.read_csv("words.csv")
+        self.word_lst = None
 
     def new(self):
+        self.word_lst = self.read_csv("words.csv")
         self.load_screen()
-        self.run()
 
     def run(self):
         while self.playing:
@@ -95,16 +85,16 @@ class Game:
             self.update()
             self.draw()
 
-        self.end_screen()
-
     def events(self):
         events = pg.event.get()
         for event in events:
             if event.type == pg.QUIT:
                 self.playing = False
+                self.running = False
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_ESCAPE:
                     self.playing = False
+                    self.running = False
                 if event.key == pg.K_SPACE:
                     self.word_lst = self.read_csv("words.csv")
                     if self.file_saved_flg:
@@ -122,35 +112,32 @@ class Game:
     def draw(self):
         self.screen.fill((255, 255, 255))
         self.txt_grp.draw(self.screen)
-        self.amy_font.render_to(self.screen, (self.screen_w - 200, self.screen_h - 50), VER + ". by Amy Yao")
+        self.amy_font.render_to(self.screen, (self.screen_w - 250, self.screen_h - 50), VER + ". by Amy Yao")
 
         if self.file_saved_flg:
             self.any_key_font.render_to(self.screen, (self.screen_centerx-150, self.screen_centery+50),
-                                        "screen saved to: " + self.file_saved_name,
-                                        bgcolor=(200, 100, 100))
+                                        "screen saved to: " + self.file_saved_name, bgcolor=(200, 100, 100))
 
         pg.display.flip()
 
     def start_screen(self):
         running = True
-        while running:
+        while running and self.running:
             self.clock.tick(FPS)
             events = pg.event.get()
             for event in events:
                 if event.type == pg.QUIT:
-                    pg.quit()
-                    exit()
+                    self.running = False
                 if event.type == pg.KEYDOWN:
                     if event.key == pg.K_ESCAPE:
-                        pg.quit()
-                        exit()
+                        self.running = False
                     else:
                         running = False
 
             self.screen.fill((255, 255, 255))
             self.splat_font.render_to(self.screen, (self.screen_centerx-330, self.screen_centery-150), "SPLAT Generator", fgcolor=(237, 105, 27))
             self.any_key_font.render_to(self.screen, (self.screen_centerx-150, self.screen_centery+50), "Press Any Key To Continue", fgcolor=(166, 214, 201))
-            self.amy_font.render_to(self.screen, (self.screen_w-200, self.screen_h-50), VER + ". by Amy Yao")
+            self.amy_font.render_to(self.screen, (self.screen_w-250, self.screen_h-50), VER + ". by Amy Yao")
 
             pg.display.flip()
 
@@ -164,24 +151,19 @@ class Game:
                     running = False
                     self.running = False
                 if event.type == pg.KEYDOWN:
-                    if event.key == pg.K_ESCAPE:
-                        running = False
-                        self.running = False
-                    else:
-                        running = False
-                        self.running = False
+                    running = False
+                    self.running = False
 
             self.screen.fill((255, 255, 255))
             self.splat_font.render_to(self.screen, (self.screen_centerx-500, self.screen_centery-150), "Enjoy, Bye For Now!", fgcolor=(237, 105, 27))
             self.any_key_font.render_to(self.screen, (self.screen_centerx-150, self.screen_centery+50), "Press Any Key To Exit", fgcolor=(166, 214, 201))
-            self.amy_font.render_to(self.screen, (self.screen_w-200, self.screen_h-50), VER + ". by Amy Yao")
+            self.amy_font.render_to(self.screen, (self.screen_w-250, self.screen_h-50), VER + ". by Amy Yao")
 
             pg.display.flip()
 
-        pg.quit()
-        exit()
-
     def load_screen(self):
+        if not self.running:
+            return
         self.txt_grp.empty()
         self.x, self.y, self.w, self.h = 50, 50, 0, 0
         word_lst = self.word_lst
@@ -207,17 +189,44 @@ class Game:
     def read_csv(self, file_name, dict=0):
         result = None
         if dict == 0:
-            with open(file_name, newline='') as csvfile:
-                reader = csv.reader(csvfile)
-                result = []
-                for item in reader:
-                    result.append(item[0])
+            try:
+                with open(file_name, newline='') as csvfile:
+                    reader = csv.reader(csvfile)
+                    result = []
+                    for item in reader:
+                        result.append(item[0])
+            except FileNotFoundError:
+                self.no_file_screen(file_name)
         elif dict == 1:
-            with open(file_name, newline='') as csvfile:
-                reader = csv.DictReader(csvfile)
-                for item in reader:
-                    print(item)
+            try:
+                with open(file_name, newline='') as csvfile:
+                    reader = csv.DictReader(csvfile)
+                    for item in reader:
+                        print(item)
+            except FileNotFoundError:
+                self.no_file_screen(file_name)
         return result
+
+    def no_file_screen(self, file_name):
+        while self.running:
+            self.clock.tick(FPS)
+            events = pg.event.get()
+            for event in events:
+                if event.type == pg.QUIT:
+                    self.running = False
+                    self.playing = False
+                if event.type == pg.KEYDOWN:
+                    self.running = False
+                    self.playing = False
+
+            self.screen.fill((255, 255, 255))
+            self.any_key_font.render_to(self.screen, (self.screen_centerx-200, self.screen_centery-50),
+                                        f"File {file_name} is not found.", fgcolor=(166, 214, 201))
+            self.any_key_font.render_to(self.screen, (self.screen_centerx-150, self.screen_centery+50),
+                                        "Press Any Key To Exit", fgcolor=(166, 214, 201))
+            self.amy_font.render_to(self.screen, (self.screen_w-250, self.screen_h-50), VER + ". by Amy Yao")
+
+            pg.display.flip()
 
 
 g = Game()
@@ -225,4 +234,6 @@ g.start_screen()
 while g.running:
     g.new()
     g.run()
-    g.end_screen()
+    # g.end_screen()
+
+pg.quit()
